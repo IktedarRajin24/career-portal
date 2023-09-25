fdc46265731893e61bfab35bf4023db820687ee5
MD. Iktedar Hasan Rushdi <rushdi2410@gmail.com>
2023-09-25 19:49:15 +0600

    header and footer responsive

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='footer bg-gray-900 text-white py-32 px-0 h-max'>
-            <div className='px-5 py-8 w-3/5 mx-auto flex justify-between  items-start border-b-2 border-slate-600'>
-                <div className='company-details w-1/3'>
+        <div className='footer bg-gray-900 text-white md:py-32 py-20 px-0 h-max'>
+            <div className='px-5 md:py-8 py-0 md:w-3/5 w-full mx-auto md:flex justify-between  items-start grid grid-cols-2 border-b-2 border-slate-600'>
+                <div className='company-details md:w-1/3 w-11/12 col-span-2 md:my-0 my-10'>
                     <h1 className='text-2xl font-bold pb-10'>BdJobs24</h1>
                     <p className='text-sm w-9/12 text-slate-400 pb-5'>There are many variations of passages of Lorem Ipsum, but the majority have suffered alteration in some form.</p>
                     <img className='w-1/3' src="assets/icons/Group 9969.png" alt="" />
                 </div>
-                <div className='about ms-10'>
+                <div className='about md:ms-10 md:my-0 my-10'>
                     <h1 className='font-semibold text-lg pb-5'>
                         Company
                     </h1>
@@ -20,7 +20,7 @@ const Footer = () => {
                     <p className='text-sm  text-slate-400'>Careers</p>
 
                 </div>
-                <div className='about ms-10'>
+                <div className='about md:ms-10 md:my-0 my-10'>
                     <h1 className='font-semibold text-lg pb-5'>
                         Company
                     </h1>
@@ -30,7 +30,7 @@ const Footer = () => {
                     <p className='text-sm  text-slate-400'>Careers</p>
 
                 </div>
-                <div className='support ms-10'>
+                <div className='support md:ms-10 md:my-0 my-10'>
                     <h1 className='font-semibold text-lg pb-5'>
                         Support
                     </h1>
@@ -40,7 +40,7 @@ const Footer = () => {
                     <p className='text-sm  text-slate-400'>Developers</p>
 
                 </div>
-                <div className='contact ms-10'>
+                <div className='contact md:ms-10 md:my-0 my-10'>
                     <h1 className='font-semibold text-lg pb-5'>
                         Contact
                     </h1>
@@ -49,7 +49,7 @@ const Footer = () => {
 
                 </div>
             </div>
-            <div className='w-3/5 mx-auto pt-10 text-xs text-slate-400 flex justify-between'>
+            <div className='w-11/12 mx-auto pt-10 text-xs text-slate-400 flex justify-between'>
                 <p>©2023 <span className='font-semibold'>BdJobs24</span>. All Rights Reserved</p>
                 <p>Powered By <span className='font-semibold'>Rushdi</span></p>
             </div>
--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,43 +1,52 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation, NavLink } from 'react-router-dom';
 import './Header.css';
 import ActiveLink from '../ActiveLink/ActiveLink';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
     const location = useLocation();
     const path = location.pathname;
+    const [open, setOpen] = useState(false);
     return (
         <div className='header'>
-            <nav className='nav flex justify-between items-center px-5 py-8 w-3/5 mx-auto'>
-                <div className="logo text-2xl font-bold">
+            <nav className='nav flex justify-between items-center md:px-5 py-8 md:w-3/5 mx-auto'>
+                <div className="logo text-2xl ps-5 font-bold">
                     <p>BdJobs24</p>
                 </div>
-                <div className='flex justify-between w-2/5'>
+                <div className={`flex md:flex-row md:items-center items-start md:gap-0 gap-3 md:bg-transparent bg-purple-50 md:p-0 px-5 pt-2 pb-10 md:w-9/12 w-full flex-col justify-between absolute md:static ${open? 'top-16':'-top-96'}`}>
                     <ActiveLink to='/'>Home</ActiveLink>
                     <ActiveLink to='/statistics'>Statistics</ActiveLink>
                     <ActiveLink to='/applied-jobs'>Applied Jobs</ActiveLink>
                     <ActiveLink to='/blog'>Blog</ActiveLink>
+                    <button className='apply-btn md:ms-44 px-4 py-3 text-xs rounded-lg text-white font-bold'>Start Applying</button>
 
                 </div>
-                <button className='apply-btn px-4 py-3 rounded-lg text-white font-bold'>Start Applying</button>
+                
+                <div onClick={()=>setOpen(!open)} className='md:hidden pe-5'>
+                    {
+                        open? <XMarkIcon className="h-8 w-8 text-purple-900"></XMarkIcon>:<Bars3Icon className="h-8 w-8 text-purple-900"></Bars3Icon>
+                    }
+
+                </div>
             </nav>
             {
                 path === '/'? 
-                <div className='banner flex items-center justify-between px-5 pt-8 w-3/5 mx-auto'>
-                    <div className="text-section w-1/2">
-                        <h1 className='text-6xl w-9/12 font-bold heading'> 
+                <div className='banner flex md:flex-row flex-col items-center justify-between px-5 pt-8 md:w-3/5 w-11/12 mx-auto'>
+                    <div className="text-section md:w-1/2 w-11/12">
+                        <h1 className='md:text-6xl text-3xl md:w-9/12 w-11/12 font-bold heading'> 
                             One Step Closer To Your <span className='dream-job'>Dream Job</span>
                         </h1>
-                        <p className='text-slate-400 mt-8 w-4/5'>
+                        <p className='text-slate-400 md:text-md text-sm mt-8 w-4/5'>
                             Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
                         </p>
-                        <button className='apply-btn mt-8 px-4 py-3 rounded-lg text-white font-bold'>
+                        <button className='apply-btn mt-8 md:px-4 px-2 md:py-3 py-2 rounded-lg text-white font-bold'>
                             Get Started
                         </button>
                     </div>
-                    <div className="image-section w-1/2">
-                        <img src="assets/images/P3OLGJ1 copy 1.png" alt="user" />
+                    <div className="image-section md:w-1/2 w-full flex  md:flex-row flex-col items-end">
+                        <img className='md:w-full w-1/2' src="assets/images/P3OLGJ1 copy 1.png" alt="user" />
                     </div>
 
                 </div>: 
