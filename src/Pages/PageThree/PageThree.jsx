056acd416c07358149bee3f1d4781f3392976a54
MD. Iktedar Hasan Rushdi <rushdi2410@gmail.com>
2023-09-25 16:24:57 +0600

    filter done

--- src/Pages/PageThree/PageThree.jsx
+++ src/Pages/PageThree/PageThree.jsx
@@ -1,23 +1,41 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { appliedJobs } from '../../Utilities/appliedJobs';
 import { useLoaderData } from 'react-router-dom';
 import AppliedJob from '../../Components/AppliedJob/AppliedJob';
 
 const PageThree = () => {
     const appliedJobs = useLoaderData()
+    const [filteredJobs, setFilteredJobs] = useState(appliedJobs)
+    const [filter, setFilter] = useState('');
+    const setFilterValue = (e) =>{
+        setFilter(e.target.value);
+        if(e.target.value === ''){
+            console.log(e.target.value);
+            setFilteredJobs(appliedJobs)
+            
+        }
+        else{
+            let filtered = appliedJobs.filter(appliedJob => appliedJob.type === e.target.value)
+            setFilteredJobs(filtered)
+            
+        }
+    }
     // console.log(appliedJobs)
     return (
         <div className='applied-jobs w-3/5 mx-auto py-20'>
             <div className='w-full flex justify-end'>
-                <select className='bg-gray-200 w-1/12 p-2 text-sm mb-10' name="filter" id="filter" disabled>
+                <select onChange={setFilterValue} className='bg-gray-200 w-2/12 p-2 text-sm mb-10' name="filter" id="filter">
                     <option value="">Filter By</option>
+                    <option value="On-site">On-site</option>
+                    <option value="Remote">Remote</option>
+
                 </select>
 
             </div>
             <div className='flex flex-col gap-6 '>
                 {
-                    appliedJobs.map(appliedJob => <AppliedJob 
+                    filteredJobs && filteredJobs.map(appliedJob => <AppliedJob 
                         key={appliedJob.id}
                         appliedJob={appliedJob} />)
 
